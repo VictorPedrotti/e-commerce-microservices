f82125a307891e4d1f166d080e714544ab5af400
Victor Pedrotti <victor1404pedrotti@gmail.com>
2025-06-15 17:44:21 -0300

    implementa circuit breaker no gateway e order-service com resilience4j

--- order-service/src/main/java/com/estudosjava/order_service/config/RestClienteConfig.java
+++ order-service/src/main/java/com/estudosjava/order_service/config/RestClienteConfig.java
@@ -1,8 +1,10 @@
 package com.estudosjava.order_service.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -16,12 +18,18 @@ public class RestClienteConfig {
   private String inventoryServiceUrl;
 
   @Bean
-  public InventoryClient inventoryClient() {
-    RestClient restClient = RestClient.builder()
+public InventoryClient inventoryClient(RestTemplateBuilder restTemplateBuilder) {
+    var factory = new SimpleClientHttpRequestFactory();
+    factory.setConnectTimeout(3000);
+    factory.setReadTimeout(3000);
+
+    var restClient = RestClient.builder()
             .baseUrl(inventoryServiceUrl)
+            .requestFactory(factory)
             .build();
-    var restClientAdapter =  RestClientAdapter.create(restClient);
-    var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
-    return httpServiceProxyFactory.createClient(InventoryClient.class);
+
+    var adapter = RestClientAdapter.create(restClient);
+    var proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
+    return proxyFactory.createClient(InventoryClient.class);
   }
 }
