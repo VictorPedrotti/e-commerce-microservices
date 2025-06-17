15a46c892129945791e68a5c127af63178701c79
Victor Pedrotti <victor1404pedrotti@gmail.com>
2025-06-17 10:56:48 -0300

    fix: ajustando campo skucode no model product e adicionando quantidades no inventário

--- product-service/src/main/java/com/estudosjava/product_service/dto/ProductRequestDto.java
+++ product-service/src/main/java/com/estudosjava/product_service/dto/ProductRequestDto.java
@@ -4,12 +4,13 @@ import java.math.BigDecimal;
 
 import com.estudosjava.product_service.model.Product;
 
-public record ProductRequestDto(String id, String name, String description, BigDecimal price) {
+public record ProductRequestDto(String id, String name, String description, String skuCode, BigDecimal price) {
 
   public Product toEntity() {
     Product product = new Product();
     product.setName(name);
     product.setDescription(description);
+    product.setSkuCode(skuCode);
     product.setPrice(price);
     return product;
   }
--- product-service/src/main/java/com/estudosjava/product_service/dto/ProductResponseDto.java
+++ product-service/src/main/java/com/estudosjava/product_service/dto/ProductResponseDto.java
@@ -4,13 +4,14 @@ import java.math.BigDecimal;
 
 import com.estudosjava.product_service.model.Product;
 
-public record ProductResponseDto(String id, String name, String description, BigDecimal price) {
+public record ProductResponseDto(String id, String name, String description, String skuCode, BigDecimal price) {
 
   public static ProductResponseDto from(Product product) {
     return new ProductResponseDto(
       product.getId(),
       product.getName(),
       product.getDescription(),
+      product.getSkuCode(),
       product.getPrice()
     );
   }
